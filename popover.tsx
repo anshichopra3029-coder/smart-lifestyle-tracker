99b8936bb305cc22e36e73ae9d387762afcde1db
Sajal Agrawal <sajalagrawal1818@gmail.com>
2026-04-19 22:40:03 +0530

    Add files via upload

---
+++ popover.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import * as React from "react"
+import * as PopoverPrimitive from "@radix-ui/react-popover"
+
+const Popover = PopoverPrimitive.Root
+const PopoverTrigger = PopoverPrimitive.Trigger
+
+const PopoverContent = React.forwardRef(
+  ({ className, align = "center", sideOffset = 4, ...props }: any, ref: any) => {
+    return (
+      <PopoverPrimitive.Portal>
+        <PopoverPrimitive.Content
+          ref={ref}
+          align={align}
+          sideOffset={sideOffset}
+          className={className}
+          {...props}
+        />
+      </PopoverPrimitive.Portal>
+    )
+  }
+)
+
+PopoverContent.displayName = "PopoverContent"
+
+export { Popover, PopoverTrigger, PopoverContent }
